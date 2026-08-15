38c7fe4c25a29943e505ae4d4f749bba3250da0e
Akrez <akrez.like@gmail.com>
2026-08-16 00:18:18 +0330

    services

---
+++ app/Services/BaseScraperService.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Services;
+
+use App\Enums\CaratEnum;
+use App\Models\Page;
+use App\Models\Product;
+use App\Models\Scrap;
+use App\Models\Variant;
+use Illuminate\Support\Facades\Log;
+
+abstract class BaseScraperService
+{
+    protected function logError(\Exception|\Throwable $e): void
+    {
+        Log::error($e->getMessage(), $e->getTrace());
+    }
+
+    protected function completeScrap(Scrap $scrap): bool
+    {
+        return $scrap->update(['completed_at' => now()]);
+    }
+
+    protected function completeProduct(Product $product): bool
+    {
+        return $product->update(['completed_at' => now()]);
+    }
+
+    protected function completePage(Page $page): bool
+    {
+        return $page->update(['completed_at' => now()]);
+    }
+
+    protected function startScrap(Scrap $scrap): bool
+    {
+        return $scrap->update(['started_at' => now()]);
+    }
+
+    protected function scrapHasStarted(Scrap $scrap): bool
+    {
+        return (bool) $scrap->started_at;
+    }
+
+    protected function scrapHasCompleted(Scrap $scrap): bool
+    {
+        return (bool) $scrap->completed_at;
+    }
+
+    protected function savePage(int $scrapId, int $number): ?Page
+    {
+        try {
+            return Page::updateOrCreate([
+                'scrap_id' => $scrapId,
+                'number' => $number,
+            ]);
+        } catch (\Exception $e) {
+            $this->logError($e);
+        }
+
+        return null;
+    }
+
+    protected function saveProduct(int $scrapId, int $pageId, string $externalId, array $productData): ?Product
+    {
+        try {
+            return Product::updateOrCreate([
+                'scrap_id' => $scrapId,
+                'page_id' => $pageId,
+                'external_id' => $externalId,
+            ], [
+                'title' => $productData['title'],
+                'image_url' => $productData['image_url'] ?? null,
+                'product_url' => $productData['product_url'] ?? null,
+                'page_number' => $productData['page_number'] ?? 0,
+            ]);
+        } catch (\Exception $e) {
+            $this->logError($e);
+        }
+
+        return null;
+    }
+
+    protected function saveVariant(int $productId, CaratEnum $caratEnum, string $seller, float $size, float $price): ?Variant
+    {
+        try {
+            return Variant::create([
+                'product_id' => $productId,
+                'carat' => $caratEnum->name,
+                'seller' => $seller,
+                'size' => $size,
+                'price' => $price,
+                'price_per_gram' => $price / $size,
+            ]);
+        } catch (\Exception $e) {
+            $this->logError($e);
+        }
+
+        return null;
+    }
+
+    protected function sanitizeNumber(string $string): string
+    {
+        return preg_replace('/[^\\d.]+/', '', $string);
+    }
+
+    protected function formatItem(string $title, float $size, string $url, ?string $image, string $seller, float $price): array
+    {
+        return [
+            'title' => $title,
+            'size' => $size,
+            'url' => $url,
+            'image' => $image,
+            'seller' => $seller,
+            'source' => $this->source->value,
+            'price' => ['r' => $price, 'f' => number_format($price)],
+            'pricePerGram' => ['r' => $price / $size, 'f' => number_format($price / $size)],
+        ];
+    }
+
+}
---
+++ app/Services/DigiKalaScraperService.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace App\Services;
+
+use App\Enums\CaratEnum;
+use App\Models\Page;
+use App\Models\Product;
+use App\Models\Scrap;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Http;
+
+class DigiKalaScraperService extends BaseScraperService
+{
+    protected const API_BASE = 'https://api.digikala.com';
+
+    public function analyze(Scrap $scrap): void
+    {
+        if (! $this->scrapHasStarted($scrap)) {
+            $this->createPages($scrap);
+        }
+        $this->createProducts($scrap);
+        $this->createVariants($scrap);
+        $this->completeScrap($scrap);
+    }
+
+    protected function callSearch(array $pages): array
+    {
+        return Http::pool(fn ($pool) => collect($pages)->map(fn ($page) => $pool->as($page)
+            ->withHeaders($this->getHeaders())
+            ->get(self::API_BASE.'/v1/categories/bullion/search/', [
+                'has_selling_stock' => 1,
+                'page' => $page,
+                'sort' => 7,
+            ])
+        )->toArray());
+    }
+
+    public function createPages(Scrap $scrap): void
+    {
+        $responses = $this->callSearch([1]);
+
+        $totalPages = null;
+        try {
+            foreach ($responses as $response) {
+                if ($response instanceof ConnectionException) {
+                    $this->logError($response);
+                } elseif ($response->successful()) {
+                    $totalPages = $response->json('data.pager.total_pages');
+                }
+            }
+        } catch (\Exception $e) {
+            $this->logError($e);
+        }
+
+        if (empty($totalPages)) {
+            return;
+        }
+
+        try {
+            foreach (range(1, $totalPages) as $pageNumber) {
+                $page = $this->savePage($scrap->id, $pageNumber);
+                if (isset($responses[$pageNumber])) {
+                    $this->createProductByResponse($scrap, $page, $responses[$pageNumber]);
+                }
+            }
+            $this->startScrap($scrap);
+        } catch (\Exception $e) {
+            $this->logError($e);
+        }
+    }
+
+    public function createProducts(Scrap $scrap): void
+    {
+        try {
+            $allPages = Page::where('scrap_id', $scrap->id)
+                ->whereNull('completed_at')
+                ->get()
+                ->pluck(null, 'number');
+            foreach (array_chunk($allPages->pluck('number')->toArray(), 10) as $pageNumbers) {
+                $responses = $this->callSearch($pageNumbers);
+                foreach ($responses as $pageNumber => $response) {
+                    $this->createProductByResponse($scrap, $allPages[$pageNumber], $response);
+                }
+            }
+        } catch (\Exception $e) {
+            $this->logError($e);
+        }
+    }
+
+    protected function createProductByResponse(Scrap $scrap, Page $page, mixed $response)
+    {
+        if ($response instanceof ConnectionException) {
+            $this->logError($response);
+        } elseif ($response->successful()) {
+            try {
+                foreach ($response->json('data.products', []) as $product) {
+                    $pageNumber = $response->json('data.pager.current_page');
+                    $result['ids'][] = $this->saveProduct($scrap->id, $page->id, $product['id'], [
+                        'title' => ($product['title_fa'] ?? $product['title'] ?? ''),
+                        'image_url' => ($product['images']['main']['url'][0] ?? null),
+                        'product_url' => 'https://www.digikala.com/product/'.$product['id'],
+                        'page_number' => $pageNumber,
+                    ])?->id;
+                    $this->completePage($page);
+                }
+            } catch (\Exception $e) {
+                $this->logError($e);
+            }
+        }
+    }
+
+    public function createVariants(Scrap $scrap): void
+    {
+        Product::where('scrap_id', $scrap->id)
+            ->whereNull('completed_at')
+            ->chunkById(60, function ($products) {
+                try {
+
+                    $products = $products->keyBy('id');
+
+                    $responses = Http::pool(fn ($pool) => $products->map(fn ($product) => $pool
+                        ->as($product->id)
+                        ->withHeaders($this->getHeaders())
+                        ->get(self::API_BASE."/v2/product/{$product->external_id}/")
+                    )->toArray());
+
+                    foreach ($responses as $productId => $response) {
+                        if ($response instanceof ConnectionException) {
+                        } elseif ($response->successful()) {
+                            foreach ($response->json('data.product.variants', []) as $variant) {
+                                $result['ids'] = $this->saveVariant(
+                                    $productId,
+                                    $this->extractCarat($response->json('data.product')),
+                                    ($variant['seller']['title'] ?? ''),
+                                    $this->extractSize($variant),
+                                    floatval($variant['price']['selling_price'] ?? 0)
+                                )?->id;
+                                $products[$productId]->update(['completed_at' => now()]);
+                            }
+                        }
+                    }
+                } catch (\Exception $e) {
+                    $this->logError($e);
+                }
+            });
+    }
+
+    protected function extractCarat(array $data): CaratEnum
+    {
+        foreach ($data['specifications'] as $specification) {
+            foreach ($specification['attributes'] as $attribute) {
+                if (strpos($attribute['title'], 'عیار') !== false) {
+                    foreach ($attribute['values'] as $attributeValue) {
+                        $carat = $this->sanitizeNumber($attributeValue);
+                        if ($carat) {
+                            $carat = floatval($carat);
+                            switch ($carat) {
+                                case 18:
+                                case 750:
+                                    return CaratEnum::CARAT_18;
+                                case 24:
+                                case 995:
+                                    return CaratEnum::CARAT_24;
+                                case 999:
+                                case 999.9:
+                                    return CaratEnum::CARAT_9999;
+                                default:
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return CaratEnum::CARAT_0;
+    }
+
+    protected function extractSize(array $data): ?float
+    {
+        return isset($data['size']['title'])
+            ? floatval($this->sanitizeNumber($data['size']['title']))
+            : null;
+    }
+
+    protected function getHeaders()
+    {
+        return [
+            'accept' => 'application/json, text/plain, */*',
+            'accept-language' => 'en-US,en;q=0.9,fa;q=0.8',
+            'x-web-client' => 'desktop',
+            'x-web-client-id' => 'web',
+            'x-web-optimize-response' => '1',
+        ];
+    }
+}
