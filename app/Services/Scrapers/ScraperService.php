063a46270143f6e55c93ee75e9d06446bbc8a8ff
Akrez <akrez.like@gmail.com>
2026-08-20 13:01:53 +0330

    up

--- app/Services/Scrapers/ScraperService.php
+++ app/Services/Scrapers/ScraperService.php
@@ -20,43 +20,6 @@ abstract class ScraperService
 
     const ERROR_CATCH = 5000;
 
-    public function summarize(string $scrapKey): array
-    {
-        $result = array_fill_keys(CaratEnum::names(), []);
-
-        Variant::query()
-            ->whereHas('scrap', fn ($q) => $q
-                ->where('scrap_key', $scrapKey)
-                ->whereNotNull('completed_at')
-            )
-            ->with(['product'])
-            ->whereNotNull('carat')
-            ->where('size', '>', 0)
-            ->where('price_per_gram', '>', 0)
-            ->orderBy('price_per_gram')
-            ->get()
-            ->each(function ($variant) use (&$result) {
-                $price = intval($variant->price);
-                $pricePerGram = intval($price / $variant->size);
-                $result[$variant->carat][] = [
-                    'ttl' => $variant->product->title,
-                    'siz' => $variant->size,
-                    'url' => $variant->product->product_url,
-                    'img' => $variant->product->image_url,
-                    'sel' => $variant->seller,
-                    'src' => $variant->scrap->source,
-                    //
-                    'prcr' => $price,
-                    'prcf' => number_format(intval($price / 10)),
-                    //
-                    'ppgr' => $pricePerGram,
-                    'ppgf' => number_format(intval($pricePerGram / 10)),
-                ];
-            });
-
-        return $result;
-    }
-
     public function analyze(string $scrapKey, SourceEnum $sourceEnum): void
     {
         $scrap = $this->firstOrCreateScrap($scrapKey, $sourceEnum);
