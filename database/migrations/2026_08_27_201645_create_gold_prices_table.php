47796ddd49d3b6a7267e9e0c0ac2ca00895f6c71
Akrez <akrez.like@gmail.com>
2026-08-27 23:33:45 +0330

    up

--- database/migrations/2026_08_27_201645_create_gold_prices_table.php
+++ database/migrations/2026_08_27_201645_create_gold_prices_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('prices', function (Blueprint $table) {
             $table->id();
             $table->string('source', 32);
-            $table->string('scrap_key', 32);
+            $table->string('price_key', 32)->nullable();
             $table->string('carat', 16)->default('18');
-            $table->timestamp('fetched_at');
             $table->unsignedBigInteger('price');
+            $table->timestamps();
         });
     }
 
